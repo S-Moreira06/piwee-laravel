008b56f63820ba39ab20d222f17016b70e9b12ab
S-Moreira06 <moreira.s@outlook.com>
2025-04-22 12:19:43 +0200

    header v1

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,14 +1,25 @@
+import { Button } from "./ui/button";
+
 export default function Header() {
     return (
-        <header className="bg-gray-800 text-white p-4 flex items-center gap-x-5">
-            <h1 className="text-2xl">Mon Application</h1>
-            <nav>
-                <ul className="flex space-x-4">
-                    <li><a href="/">Accueil</a></li>
-                    <li><a href="/about">À propos</a></li>
-                    <li><a href="/contact">Contact</a></li>
-                </ul>
-            </nav>
+        <header className="bg-gray-600 text-white p-4 flex justify-between items-center ">
+            <div className="flex items-center gap-x-5">
+                <h1 className="text-2xl">Piwee!</h1>
+                <nav>
+                    <ul className="flex space-x-4">
+                        <li><a href="/">Accueil</a></li>
+                        <li><a href="/">Categories</a></li>
+                        <li><a href="/">Contact</a></li>
+                    </ul>
+                </nav>
+            </div>
+            {/* <div className="flex gap-x-4">
+                <Button className="">S'inscrire</Button>
+                <Button className="">Se connecter</Button>
+            </div> */}
+            <div>
+                <Button>Mon profil</Button>
+            </div>
         </header>
     );
 }
--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -1,3 +1,9 @@
 export default function Home(){
-    return "Bonjour"
+    return (
+        <div className="bg-gray-400">
+            <h1 className="text-2xl font-bold">Bienvenue sur Piwee!</h1>
+            <p className="mt-4">Votre plateforme de gestion de projets.</p>
+            <p className="mt-2">Explorez nos fonctionnalités et commencez à organiser votre travail dès aujourd'hui!</p>
+        </div>
+    )
 }
