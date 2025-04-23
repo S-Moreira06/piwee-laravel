c9cbc86cb3bdbd0e9914d199ab842f8be2677833
S-Moreira06 <moreira.s@outlook.com>
2025-04-23 13:42:14 +0200

    card component ok

--- resources/js/pages/details.jsx
+++ resources/js/pages/details.jsx
@@ -1,5 +1,18 @@
+// ShadCN UI Import 
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+
+
 import {usePage} from "@inertiajs/react";
 import { FakeItems } from "../hooks/useFakeItems";
+
 
 export default function Details() {
     const { id } = usePage().props;
@@ -7,16 +20,25 @@ export default function Details() {
     const shirt = shirts[id-1]; 
 
     return (
-        <div className="bg-gray-400 h-[1800px]">
-            <h1 className="text-2xl font-bold place-self-center">Détails du produit</h1>
-            <div className="flex justify-center items-center mt-4">
-                <img src={shirt.image} alt={shirt.name} className="w-1/3" />
-                <div className="ml-4">
-                    <h2 className="text-xl font-bold">{shirt.name}</h2>
-                    <p className="text-gray-600">${shirt.price}</p>
-                    <p className="mt-2">Description du produit...</p>
-                </div>
-            </div>
+        <div className="bg-gray-400 h-[1800px] p-2">
+            <Card className="w-3/4 place-self-center bg-gray-500 border-gray-600 shadow-2xl">
+                <CardHeader>
+                    <CardTitle className="place-self-center">{shirt.name}</CardTitle>
+                    <CardDescription className="place-self-center">Marque</CardDescription>
+                </CardHeader>
+                <CardContent className="flex p-1.5">
+                    <img src={shirt.image} alt={shirt.name} className="w-2/3  p-1.5 rounded-2xl" />
+                    <div className="flex flex-col justify-between h-auto">
+                        <p>Description du produit...</p>
+                        <div>
+                            <p className="">${shirt.price}</p>
+                            <Button className="" variant="outline">Ajouter au panier</Button>
+                        </div>
+                    </div>
+                </CardContent>
+
+            </Card>
+            
         </div>
     )
 }
