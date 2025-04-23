34959779d63574dec828805c07585c54c3dd1846
S-Moreira06 <moreira.s@outlook.com>
2025-04-23 13:45:54 +0200

    fix header link spa

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -1,15 +1,17 @@
 import { Button } from "./ui/button";
 
+import { usePage, Link, router} from '@inertiajs/react';
+
 export default function Header() {
     return (
         <header className="bg-gray-600 text-white p-2 sm:p-4 flex sticky top-0 justify-between items-center z-10 ">
             <div className="flex items-center gap-x-5">
-                <h1 className="text-2xl">Piwee!</h1>
+                <h1 className="text-2xl"><Link href={'/'}>Piwee!</Link></h1>
                 <nav className="hidden sm:block">
                     <ul className="flex space-x-1 sm:space-x-4">
-                        <li><a href="/">Accueil</a></li>
-                        <li><a href="/">Categories</a></li>
-                        <li><a href="/">Contact</a></li>
+                        <li><Link href={"/"}>Accueil</Link></li>
+                        <li><Link href={"/"}>Categories</Link></li>
+                        <li><Link href={"/"}>Contact</Link></li>
                     </ul>
                 </nav>
             </div>
