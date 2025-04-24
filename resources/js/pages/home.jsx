55ad3555ef531948a3ccbeedf76ec39bc16873cf
S-Moreira06 <moreira.s@outlook.com>
2025-04-24 13:35:43 +0200

    font test ok

--- resources/js/pages/home.jsx
+++ resources/js/pages/home.jsx
@@ -10,12 +10,12 @@ export default function Home(){
             <Hero />
             <div className="max-md:flex mx-2">
                 <div>
-                    <h1 className="text-2xl font-bold place-self-center">T-shirt</h1>
+                    <h1 className="text-2xl font-bold place-self-center joti">T-shirt</h1>
                     <MainCarousel data={shirts} />
                     
                 </div>
                 <div>
-                    <h1 className="text-2xl font-bold place-self-center md:mt-3">Sneakers</h1>
+                    <h1 className="text-2xl font-bold place-self-center md:mt-3 joti">Sneakers</h1>
                     <MainCarousel data={shoes} />
                 </div>
             </div>
