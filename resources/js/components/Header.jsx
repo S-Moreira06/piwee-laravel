cec43725756105e4c386aa0831475b2705296c4f
S-Moreira06 <moreira.s@outlook.com>
2025-04-28 09:51:47 +0200

    fix header home route

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
     return (
         <header className="bg-gray-600 text-white p-2 sm:p-4 flex sticky top-0 justify-between items-center z-10 ">
             <div className="flex items-center gap-x-5">
-                <h1 ><Link href={'/'} className="text-2xl joti">Piwee!</Link></h1>
+                <h1 ><Link href={route("home")} className="text-2xl joti">Piwee!</Link></h1>
                 <nav className="hidden sm:block">
                     <ul className="flex space-x-1 sm:space-x-4">
                         <li><Link href={"/"} className="">Accueil</Link></li>
