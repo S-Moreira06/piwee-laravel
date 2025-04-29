90fc4bcabc636b0b27ae026d4f5c347fec14008e
S-Moreira06 <moreira.s@outlook.com>
2025-04-29 14:50:31 +0200

    add daisyui card component to caroussel

--- resources/js/components/MainCarousel.jsx
+++ resources/js/components/MainCarousel.jsx
@@ -15,12 +15,33 @@ export default function MainCarousel (props) {
         <Carousel orientation={orientation} opts={{ align: "start",loop: true }} className="">
             <CarouselContent className="max-md:h-105 ml-0 max-md:mt-0 max-md:mb-1 md:mr-2.5">
             {randomItems.map((item) => (
-                <CarouselItem key={item.id} className="bg-white p-2 m-0.5 rounded shadow md:basis-1/3 lg:basis-1/5">
-                    <Link href={`/details/${item.id}`} >
+                <CarouselItem key={item.id} className=" rounded shadow-xl md:basis-1/3 lg:basis-1/5">
+                    {/* <Link href={`/details/${item.id}`}>
                         <img src={item.image} alt={item.name} className="" />
                         <h2 className="text-gray-600">{item.name}</h2>
                         <p className="text-gray-600">{item.price}€</p>
-                    </Link> 
+                    </Link>  */}
+                    <div className="card bg-base-100 ">
+                        <figure>
+                            <img
+                            src={item.image}
+                            alt="Shoes" />
+                        </figure>
+                        <div className="card-body">
+                            <div className="flex w-auto justify-between">
+                                <h2 className="card-title text-lg text-nowrap">{item.name}</h2>
+                                <span className="text-bold text-lg">{item.price}€</span>
+                            </div>
+                            <p>
+                            {item.description && item.description.length > 100
+                                ? item.description.slice(0, 50) + '...'
+                                : item.description}
+                            </p>
+                            <div className="card-actions justify-end">
+                            <button className="btn btn-primary">Voir détails</button>
+                            </div>
+                        </div>
+                    </div>
                 </CarouselItem>
             ))}
             </CarouselContent>
