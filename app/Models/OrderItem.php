68e9ae5495874398dd0bc6ebf4d0169069cede0e
S-Moreira06 <moreira.s@outlook.com>
2025-06-15 16:15:01 +0200

    models and migrations for order and order item

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class OrderItem extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'order_id', 'item_id', 'name', 'total_price', 'quantity'
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
+}
---
+++ database/migrations/2025_06_15_140908_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->string('reference', 8);
+            $table->decimal('shipping');
+            $table->decimal('total');
+            $table->decimal('tax');
+            $table->string('invoice_number', 40)->nullable();
+            $table->string('status', 10); // Champ pour l’état de la commande
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
