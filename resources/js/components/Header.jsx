4d0881ef2844a4c042d5f69b8695d77ccb0abf61
S-Moreira06 <moreira.s@outlook.com>
2025-10-31 14:13:32 +0100

    adjust

--- resources/js/components/Header.jsx
+++ resources/js/components/Header.jsx
@@ -37,7 +37,6 @@ export default function Header() {
                 </ul>
             </div>
             <div className="navbar-end gap-3">
-
                 {auth.user === null ? (
                     <>
                         <Link href={route('auth.login')} className="btn btn-secondary ">Connexion</Link>
@@ -48,66 +47,10 @@ export default function Header() {
                         <Link href={route('cart.index')} className="montserrat-semibold-italic">Panier</Link>
                         <Link href={route('profile')} className="btn btn-primary ">Profil</Link>
                         <Link href={route('auth.logout')} className="btn btn-primary ">Deco</Link>
-                    
                     </>
-
                 )}
                 
             </div>
         </header>
-
-        //     ANCENNE VERSION SHADCNUI
-        // <header className="bg-gray-600 text-white p-2 sm:p-4 flex sticky top-0 justify-between items-center z-10 ">
-        //     <div className="flex items-center gap-x-5">
-        //         <h1 >
-        //             <Link href={route("home")} className="text-3xl md:text-6xl joti">Piwee!</Link>
-        //         </h1>
-        //         {/* <NavigationMenu className="text-lg">
-        //             <NavigationMenuList>
-        //                 <NavigationMenuItem>
-        //                     <NavigationMenuTrigger className="text-lg md:text-xl">Categories</NavigationMenuTrigger>
-        //                     <NavigationMenuContent className="px-7">
-        //                         <Link href={route("category.index", { id: 1 })}>
-        //                             <NavigationMenuLink className="text-lg md:text-xl px-2">T-Shirts</NavigationMenuLink>
-        //                         </Link>
-        //                         <Link href={route("category.index", { id: 2 })}>
-        //                             <NavigationMenuLink className="text-lg md:text-xl px-2">Sneakers</NavigationMenuLink>
-        //                         </Link>
-        //                     </NavigationMenuContent>
-        //                 </NavigationMenuItem>
-        //                 <NavigationMenuItem>
-        //                     <Link href={route("contact")}>
-        //                         <NavigationMenuLink className="text-lg md:text-xl">Contact</NavigationMenuLink>
-        //                     </Link>
-        //                 </NavigationMenuItem>
-        //             </NavigationMenuList>
-        //         </NavigationMenu> */}
-
-        //         {/* <nav className="hidden sm:block">
-        //             <ul className="flex space-x-1 sm:space-x-4">
-        //                 <li><Link href={"/"} className="">Accueil</Link></li>
-        //                 <li><Link href={"/"}>Categories</Link></li>
-        //                 <li><Link href={route("contact")}>Contact</Link></li>
-        //             </ul>
-        //         </nav> */}
-        //     </div>
-            
-        //     {/* <div>
-        //         <Link href={route('cart.index')}>
-        //             <Button className="mr-1 text-lg md:text-xl">Panier</Button>
-        //         </Link>
-        //         <Link href={route('auth.login')}>
-        //             <Button className="mr-1 text-lg md:text-xl">Connexion</Button>
-        //         </Link>
-        //         <Link href={route('auth.register')}>
-        //             <Button className="text-lg md:text-xl">Inscription</Button>
-        //         </Link>
-        //     </div> */}
-        //     <div className="flex gap-3">
-        //         <Link href={route('cart.index')} className="btn">Panier</Link>
-        //         <Link href={route('auth.login')} className="btn btn-primary">Connexion</Link>
-        //         <Link href={route('auth.register')} className="btn btn-primary">Inscription</Link>
-        //     </div>
-        // </header>
     );
 }
--- resources/js/layouts/layout.jsx
+++ resources/js/layouts/layout.jsx
@@ -7,6 +7,7 @@ export default function Layout({ children }) {
     return (
         <div>
             <Header />
+            <Divider />
                 <main className='min-h-screen'>{children}</main>
             <Divider />
             <Footer />
